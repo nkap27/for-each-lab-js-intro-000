daa2a69dd4b90593f5e67bffa344331167fb4a82
Natasha K <kapr.natasha@gmail.com>
2018-06-11 16:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function iterativeLog(array){
-  array.forEach((element, index) => {
-    console.log(`${index}: ${element}`);
-  });
 
-// function iterate(array){
-//   array.forEach(forEach);
-//   return array;
-// }
+
+// function iterativeLog(array){
+//   array.forEach((element, index) => {
+//     console.log(`${index}: ${element}`);
+//   });
+//
+// // function iterate(array){
+// //   array.forEach(forEach);
+// //   return array;
+// // }
