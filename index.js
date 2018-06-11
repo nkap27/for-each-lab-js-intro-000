ff843d27a7878decb747e8b2ed4ddbc41f22e0c5
Natasha K <kapr.natasha@gmail.com>
2018-06-11 16:08:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  });
+}
 
 // function iterativeLog(array){
 //   array.forEach((element, index) => {
